83b7bbf8b8a5664aafd58bd611633a4775978dd6
Claire Carroll <carroll.claire.e@gmail.com>
2021-12-06 11:58:32 -0800

    Add workouts query

---
+++ workouts.sql
@@ -0,0 +1,58 @@
+with pedometer_intervals as (
+    select * from analytics.dbt_claire.pedometer_intervals
+),
+
+jump_sections as (
+    select * from analytics.dbt_claire.jump_sections
+),
+
+break_sections as (
+    select * from analytics.dbt_claire.break_sections
+),
+
+workout_streaks as (
+    select
+        workout_id,
+        sum(total_seconds) as total_jumping_time,
+        count(*) as n_streaks,
+        avg(total_steps) as average_steps,
+        median(total_steps) as median_steps,
+        max(total_steps) as best_streak
+
+    from jump_sections
+    group by 1
+),
+
+workout_breaks as (
+    select
+        workout_id,
+        sum(total_seconds) as total_break_time,
+        count(*) as n_breaks
+
+    from break_sections
+    group by 1
+),
+
+workouts_aggregated as (
+    select
+        workout_id,
+        min(interval_start_at) as start_at,
+        max(interval_end_at) as end_at,
+        sum(steps_this_interval) as total_steps,
+        sum(seconds_this_interval) as total_seconds
+
+    from pedometer_intervals
+    group by 1
+),
+
+joined as (
+    select
+        *,
+        1.0 * total_jumping_time / total_seconds as jumping_ratio
+
+    from workouts_aggregated
+    left join workout_streaks using (workout_id)
+    left join workout_breaks using (workout_id)
+)
+
+select * from joined
